789eefdf3f8adaf8af904a7e04d64ec7b388fdad
Corné den Breejen <cornedev@outlook.com>
2022-01-19 09:21:00 +0100

    Initial commit

---
+++ SchoolAPI/Controllers/TeachersController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolAPI.Models;
+using System.Linq;
+
+namespace SchoolAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public TeachersController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult AllTeachers()
+        {
+            return Ok(_context.Teachers.Include(x => x.Students));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult SingleTeacher(int id)
+        {
+            Teacher teacher = _context.Teachers.Include(t => t.Students).FirstOrDefault(t => t.Id == id);
+
+            if(teacher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teacher);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTeacher([FromBody] Teacher newTeacher)
+        {
+
+            _context.Teachers.Add(newTeacher);
+            _context.SaveChanges();
+
+            return Ok(newTeacher);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTeacher(int id, [FromBody] Teacher newValues)
+        {
+            Teacher teacher = _context.Teachers.Include(t => t.Students).FirstOrDefault(t => t.Id == id);
+
+            if(teacher == null)
+            {
+                return NotFound();
+            }
+
+            teacher.Name = newValues.Name;
+            teacher.Course = newValues.Course;
+            _context.SaveChanges();
+
+            return Ok(teacher);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTeacher(int id)
+        {
+            Teacher teacher = _context.Teachers.FirstOrDefault(t => t.Id == id);
+
+            if(teacher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teachers.Remove(teacher);
+            _context.SaveChanges();
+
+            return Ok(teacher);
+        }
+
+        [HttpGet("{id}/students")]
+        public IActionResult GetStudents(int id)
+        {
+            Teacher teacher = _context.Teachers.Include(x => x.Students).FirstOrDefault(t => t.Id == id);
+
+            if(teacher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teacher.Students);
+        }
+
+        [HttpPost("{id}/students")]
+        public IActionResult AddStudent(int id, [FromBody] Student student)
+        {
+            Teacher teacher = _context.Teachers.Include(x => x.Students).FirstOrDefault(t => t.Id == id);
+
+            if(teacher == null)
+            {
+                return NotFound();
+            }
+
+            teacher.Students.Add(student);
+            _context.SaveChanges();
+
+            return Ok(student);
+        }
+    }
+}
+
+// GET -> Ophalen van info
+// POST -> Versturen van info
+// DELETE -> Verwijderen van een entity
+// PUT/PATCH -> Updaten van een enitity
---
+++ SchoolAPI/Models/SchoolContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace SchoolAPI.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Teacher Teacher { get; set; }
+        // Shadow Foreign key = TeacherId
+    }
+
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public List<Student> Students { get; set; }
+    }
+
+    // Student heeft 1 docent
+    // Docent kan meerdere studenten
+}
